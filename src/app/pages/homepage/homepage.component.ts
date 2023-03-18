e08d417c08a6acf00b1ca886cd377bfe6cb0e461
Francesco <7frapar@gmail.com>
2023-03-18 18:28:11 +0100

    tailwind

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "homepage-page",
   templateUrl: "./homepage.component.html",
-  styleUrls: ["./homepage.component.html"]
+  styleUrls: ["./homepage.component.css"]
 }) export class HomePageComponent {
 
 }
