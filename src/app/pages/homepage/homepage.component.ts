c3784e959cea7da6063ff62b8e2dbdddd4df24d6
Francesco <7frapar@gmail.com>
2023-03-24 22:09:08 +0100

    erased everything

--- src/app/pages/homepage/homepage.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "homepage-page",
-  templateUrl: "./homepage.component.html",
-  styleUrls: ["./homepage.component.css"]
-}) export class HomePageComponent {
-
-}
